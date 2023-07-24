f3e669ee0d74810242909485e1a0d164fa3f3352
al3-rom <sasha.stankevich223@gmail.com>
2023-07-24 16:41:43 +0200

    Actualizando ejercicios

--- Tema1/CasaCambios/Ejercicio2.py
+++ Tema1/CasaCambios/Ejercicio2.py
@@ -1,35 +1,32 @@
 #Crear un script que introduzca Euros a dolares 1 EU = 1.2$
 #La cantidad tiene que poner usuario
 
-
+# Pido una cantidad de dinero
 DineroTxt = input("Pon una cantidad de dinero ")
-DineroNum = int(DineroTxt)
+DineroNum = float(DineroTxt)
 
+# Pongo que un $ vale 1.2
 Dolar = 1.2
 
-SumaTotal = DineroNum * Dolar
+#Hago en dolar
+EnDolar = DineroNum * Dolar
 
-print(DineroNum, "Euros, son :", SumaTotal, "$")
-
+#El resultado
+print(DineroNum, "Euros son :", EnDolar, "$")
 
 # La casa se queda un 10% en concepto de ‘tasas de gestión’. Calcula el monto
 # recibido, el cambio en dólares, la cantidad que se queda la casa de cambios y la cantidad de
 # dólares restante que recibirá el usuario. Imprime el desglose por pantalla formateado de tal
 # forma que quede claro para el usuario. 
 
+# La cantidad que se queda la casa de cambios
+tasasGestion = EnDolar * 0.1
 
-# Monto recibido
-ImpuestoEuro = DineroNum * 0.1
-print("Monto recibido: ", ImpuestoEuro, " Euros")
+# La cantidad de dólares que recibirá el usuario
+dolaresRecibidos = EnDolar - tasasGestion
 
-# Traduzco monto recibido, de euros a dolares
-ImpuestoEuro = ImpuestoEuro * Dolar
-print("Monto recibido en dolares: ", ImpuestoEuro, "$")
-
-#El cambio en dolares
-ElCambio = SumaTotal - ImpuestoEuro
-
-print("El cambio es :", ElCambio, "$")
-
-##No entiendo el ejercicio, acabo luego :(
-
+# Imprimir el desglose de la operacion
+print("Monto ingresado: ", DineroNum, "euros")
+print("Cambio en dolares: ", EnDolar, "$")
+print("La tasa de gestion: ", tasasGestion, "$")
+print("La cantidad que recibe el usuario: ", dolaresRecibidos, "$")
